adc6d05bbac7b346edf7a47969ca3ed1f8222d65
Minjeong Park <mg7694@naver.com>
2026-04-28 15:45:34 +0200

    Implement FlexSalesBookStore

--- Lab03/bookStore/src/bookStore/FlexSalesBookStore.java
+++ Lab03/bookStore/src/bookStore/FlexSalesBookStore.java
@@ -1,5 +1,35 @@
 package bookStore;
 
-public class FlexSalesBookStore {
-
+public class FlexSalesBookStore extends BookStore{
+	
+	private FlexDiscount discount;
+	
+	public FlexSalesBookStore(FlexDiscount f) {
+		this.discount = f;
+	}
+	
+	public void setDiscount(FlexDiscount d) {
+		discount = d;
+	}
+	
+	public FlexDiscount getDiscount() {
+		return discount;
+	}
+	
+	@Override
+	public void addBook(String author, String title, double basePrice) {
+		Book book = new Book(author, title, basePrice);
+		double dPerc = discount.getDiscount(book);
+		
+		Book bookFinal;
+		
+		if(dPerc > 0) {
+			bookFinal = new BookOnSale(author, title, basePrice, dPerc);
+		}
+		else {
+			bookFinal = book;
+		}
+		
+		super.addBook(bookFinal);
+	}
 }
